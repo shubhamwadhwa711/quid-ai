d09693718c3f25f557555e67019be87311a648c6
anuragdas2001 <anuragdas12921@gmail.com>
2025-02-27 16:29:34 +0530

    feat (Homepage) : linked manifest file

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,8 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Link the PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        
+        {/* Set a theme color */}
+        <meta name="theme-color" content="#F97316" />
+
+        {/* Enable standalone PWA mode */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* Apple Touch Icon (for iOS) */}
+        <link rel="apple-touch-icon" href="/quid-icon.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
