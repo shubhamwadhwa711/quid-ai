b82c36ca50927158e200598f28a19587e32e610d
anuragdas2001 <anuragdas12921@gmail.com>
2025-02-27 16:28:24 +0530

    feat (Homepage) : added quidai logo for splash screen

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,31 @@
-import { BottomNav } from "@/components/bottom-nav";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Navbar from "@/components/navbar";
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import { BottomNav } from "@/components/bottom-nav";
+import Navbar from "@/components/navbar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setShowSplash(false), 2000); // Show splash screen for 2 seconds
+  }, []);
+
   return (
-    <div>
-      <Navbar />
-      <Component {...pageProps} />
-      <BottomNav />
-    </div>
+    <Provider store={store}>
+      {showSplash ? (
+        <div className="flex items-center justify-center h-screen bg-black">
+          <img src="/quid-icon.png" alt="Splash Screen" className="w-64" />
+        </div>
+      ) : (
+        <div className="bg-gradient-to-r from-black via-blue-950 to-black min-h-screen">
+          <Navbar />
+          <Component {...pageProps} />
+          <BottomNav />
+        </div>
+      )}
+    </Provider>
   );
 }
