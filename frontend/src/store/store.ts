35c870bf1cb9671165b7df41e703dccb4982ba15
anuragdas2001 <anurag.das@shubpy.com>
2025-03-04 17:09:36 +0530

    feat (homepage) : updated reducer store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import companyReducer from "../reducers/companySlice"; // Import company reducer
-
+import companyReducer from "../reducers/company/companySlice"; // Import company reducer
+import companyCategoryReducer from "../reducers/company/category/companycategorySlice"; // Import company category reducer
+import FAQReducer from "@/reducers/faq/faqSlice";
+import InsightCategoryReducer from "@/reducers/insights/category/insightscategorySlice";
+import InsightsReducer from "@/reducers/insights/insightsSlice";
 export const store = configureStore({
   reducer: {
     company: companyReducer,
+    companyCategory: companyCategoryReducer,
+    FAQ: FAQReducer,
+    insights: InsightsReducer,
+    insightsCategory: InsightCategoryReducer,
   },
 });
 
