1d981c5b5b83140ca709052225e9faffcb57f4a3
mahimaverma <shubhamwadhwa711@gmail.com>
2025-03-05 11:06:21 +0530

    refactor : removed jwt url

--- Backend/apps/user/urls.py
+++ Backend/apps/user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . views import *
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 
 urlpatterns = [
    
     path("home/", Home.as_view(), name="home"),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/login', LoginView.as_view(), name='auth_login'),
     
 ]
