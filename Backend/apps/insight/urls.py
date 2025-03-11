bb13ef61bc3f6699b4c8655b5b1a75998df4b7bd
mahimaverma <shubhamwadhwa711@gmail.com>
2025-03-11 14:26:06 +0530

    refactor(insight):updated urls name

--- Backend/apps/insight/urls.py
+++ Backend/apps/insight/urls.py
@@ -8,7 +8,7 @@ router.register(r'category', CompanyCategoryViewSet)
 router.register(r'all-company', AllCompany)
 router.register(r'company', AssociatedCompanyViewSet, basename='company')
 router.register(r'testimonial', Testimonial)
-router.register(r'all-insight', AllInsightViewSet, basename='all-insight')
+router.register(r'all-insight-category', AllInsightViewSet, basename='all-insight')
 router.register(r'insight-category', InsightsCategoryViewSet)
 router.register(r'insight', InsightsViewSet, basename = 'insight')
 
