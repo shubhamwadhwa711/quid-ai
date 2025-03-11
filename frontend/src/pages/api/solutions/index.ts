1a4a330c4ba7add072309e568425a4e63d1742eb
anuragdas2001 <anurag.das@shubpy.com>
2025-03-11 16:49:53 +0530

    feat (homepage) : updated api proxy handling

--- frontend/src/pages/api/solutions/index.ts
+++ frontend/src/pages/api/solutions/index.ts
@@ -6,7 +6,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get(`${process.env.BACKEND_URL}/faq`, {
+    const response = await axios.get(`${process.env.BACKEND_URL}/industry`, {
       headers: { "Content-Type": "application/json" },
     });
 
