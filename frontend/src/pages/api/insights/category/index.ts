a9a2030d06893f006f47d7ad6ba83d57e020cf65
anuragdas2001 <anurag.das@shubpy.com>
2025-03-13 17:19:12 +0530

    feat (homepage) : added insights category api

--- frontend/src/pages/api/insights/category/index.ts
+++ frontend/src/pages/api/insights/category/index.ts
@@ -6,7 +6,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get(`${process.env.BACKEND_URL}/all-insight-category`, {
+    const response = await axios.get(`${process.env.BACKEND_URL}/insight-category`, {
       headers: { "Content-Type": "application/json" },
     });
 
