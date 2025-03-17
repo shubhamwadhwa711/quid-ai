f3fae9604ed5d228d8d7989c1f2203f745c5f2c5
anuragdas2001 <anurag.das@shubpy.com>
2025-03-17 13:52:50 +0530

    feat (homepage) : updated home

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,20 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Home from "@/components/Home";
 export default function Component() {
-  const { data: session } = useSession();
-  const handleSocialLogin = (provider: string) => {
-    signIn(provider, { callbackUrl: "/home" });
-  };
-  console.log("session index", session);
-  if (!session) {
-    return (
-      <div className="flex justify-center flex-col items-center p-40">
-        Not signed in <br />
-        <button onClick={() => handleSocialLogin("linkedin")}>
-          Sign in with LinkedIn
-        </button>
-      </div>
-    );
-  }
+  // const { data: session } = useSession();
+  // const handleSocialLogin = (provider: string) => {
+  //   signIn(provider, { callbackUrl: "/home" });
+  // };
+  // console.log("session index", session);
+  // if (!session) {
+  //   return (
+  //     <div className="flex justify-center flex-col items-center p-40">
+  //       Not signed in <br />
+  //       <button onClick={() => handleSocialLogin("linkedin")}>
+  //         Sign in with LinkedIn
+  //       </button>
+  //     </div>
+  //   );
+  // }
   return <Home />;
 }
