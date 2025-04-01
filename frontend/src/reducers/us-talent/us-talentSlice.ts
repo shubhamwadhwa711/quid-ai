76142206389f83a6065f189df8b10b044faa080b
anuragdas2001 <anurag.das@shubpy.com>
2025-04-01 17:26:31 +0530

    feat (homepage) : updated reducers

---
+++ frontend/src/reducers/us-talent/us-talentSlice.ts
@@ -0,0 +1,127 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
+interface Industry {
+  id: number;
+  name: string;
+  logo: string;
+}
+interface Country {
+  id: number;
+  name: string;
+}
+interface Education {
+  id: 1;
+  school: string;
+  degree: string;
+  field_of_study: string;
+  start_year: number;
+  end_year: number;
+  description: string;
+  profile: number;
+}
+interface Client {
+  id: number;
+  name: string;
+  client: string;
+  profile: number;
+}
+interface Project {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  url: string;
+  start_date: string;
+  end_date: string;
+  profile: number;
+}
+interface Skill {
+  id: number;
+  name: string;
+}
+interface Availability {
+  id: number;
+  name: string;
+}
+interface Language {
+  id: number;
+  name: string;
+}
+export interface Profile {
+  id: number;
+  title: string;
+  user: User;
+  skill: Skill[];
+  education: Education[];
+  client: Client[];
+  image: string;
+  headline: string;
+  summary?: string;
+  industry: Industry;
+  country: Country;
+  linkedin_url: string;
+  projects: Project[];
+  available_to?: Availability[];
+  languages?: Language[];
+}
+
+interface ProfileState {
+  usprofile: Profile[];
+  loading: boolean;
+  error: string | null;
+}
+
+// Initial state
+const initialState: ProfileState = {
+  usprofile: [],
+  loading: false,
+  error: null,
+};
+
+// Async Thunk to fetch company data
+export const fetchUSProfile = createAsyncThunk(
+  "profile/fetchUSProfile",
+  async (_, { rejectWithValue }) => {
+     console.log("Fetching US profile...", );
+    try {
+      const response = await axios.get("/api/us-talent");
+      console.log("profile fetched:", response.data);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response?.data?.message || "Failed to profile"
+      );
+    }
+  }
+);
+
+// Create the slice
+const USProfileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUSProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUSProfile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.usprofile = action.payload;
+      })
+      .addCase(fetchUSProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+  },
+});
+
+export default USProfileSlice.reducer;
