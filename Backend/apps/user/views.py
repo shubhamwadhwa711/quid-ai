d5143318aa21540a723a30b76604ada2a7844cb7
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-01 08:09:56 +0530

    refactor: deleted unused file

--- Backend/apps/user/views.py
+++
@@ -1,8 +0,0 @@
-# from django.http import HttpResponse
-# from django.shortcuts import render
-# from ..talent.tasks import send_mail_func
-# # Create your views here.
-
-# def send_mail_to_all(request): 
-#     send_mail_func.delay()  
-#     return HttpResponse("Sent Email Successfully...Check your mail please")    
