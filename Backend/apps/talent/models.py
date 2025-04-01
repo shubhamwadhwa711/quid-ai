40afb3071f24d6bf077fb415fae2c84b128cec02
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-01 12:29:34 +0530

    refactor: updated fields

--- Backend/apps/talent/models.py
+++ Backend/apps/talent/models.py
@@ -134,7 +134,7 @@ class Enquiry(models.Model):
     mobile = models.IntegerField(null=True)
     message = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by =models.ForeignKey(User, on_delete=models.CASCADE, related_name='enquiry_updated_by_user',null=True) 
+    updated_by =models.ForeignKey(User, on_delete=models.CASCADE, related_name='enquiry_updated_by_user',null=True, blank=True) 
     
     def __str__(self):
         return self.full_name   
