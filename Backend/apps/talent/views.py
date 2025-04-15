2e1027e3c85081cde1272542056d400b6da0e461
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-15 18:12:00 +0530

    refactor: Implemented permission

--- Backend/apps/talent/views.py
+++ Backend/apps/talent/views.py
@@ -26,12 +26,12 @@ class ProfileViewSet(viewsets.ModelViewSet):
     """
     API view to list current user.
     """
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    # queryset = Profile.objects.all()
 
-    # def get_queryset(self):
-    #     return Profile.objects.filter(user=self.request.user)
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
 
     # def get_serializer_class(self):
     #     print(self.request.method)
