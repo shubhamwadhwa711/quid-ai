c802dcd2fad1d849a21d1f9b511df74de987693b
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-15 14:03:24 +0530

    refactor: updated view

--- Backend/apps/talent/views.py
+++ Backend/apps/talent/views.py
@@ -26,11 +26,12 @@ class ProfileViewSet(viewsets.ModelViewSet):
     """
     API view to list current user.
     """
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = ProfileSerializer
+    queryset = Industry.objects.all()
 
-    def get_queryset(self):
-        return Profile.objects.filter(user=self.request.user)
+    # def get_queryset(self):
+    #     return Profile.objects.filter(user=self.request.user)
 
     # def get_serializer_class(self):
     #     print(self.request.method)
