27676531f6fdf8498a6aa5f4460a158bfcd2daee
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-16 12:28:13 +0530

    refactor: uncommented signals

--- Backend/apps/talent/signals.py
+++ Backend/apps/talent/signals.py
@@ -27,14 +27,14 @@ def send_approval_email(sender, instance, **kwargs):
         send_mail_enq.delay(subject, message, sender_email, recipient_email)    
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
  
        
