7fd43ea7b2da12a717f52631fec080ec359c1d2a
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-21 16:30:18 +0530

    refactor: updated settings

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -263,12 +263,12 @@ ACTIVATE_JWT = True
 
 # CELERY SETTINGS
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_Task_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Kolkata'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 
 
