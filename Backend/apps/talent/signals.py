d9a7ac28a7926991985a8187a1f60a145d2b87aa
mahimaverma <shubhamwadhwa711@gmail.com>
2025-04-22 18:52:35 +0530

    refactor: updated signals

--- Backend/apps/talent/signals.py
+++ Backend/apps/talent/signals.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile, Enquiry
-from .tasks import send_mail_enq , send_mail_talent
+from .tasks import  send_mail_talent
 
 
 @receiver(post_save, sender= Profile)
-def profile_approve(sender, instance, created, **kwargs):
+def profile_approve( sender, instance, created, **kwargs):
     if instance.status == "APPROVED":
         subject = "Congratulations " + instance.user.first_name + " your profile has been Approved"
         message = "Congratulations " + instance.user.first_name + " your profile has been Approved. You are now ready to fly!"
@@ -18,14 +18,18 @@ def profile_approve(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Enquiry)
 def send_approval_email(sender, instance, **kwargs):
-    if instance.status == "APPROVED":  
+    print("Hello")
+    if instance.status == "APPROVED": 
+        print("Hello") 
         subject = f"Hey {instance.profile.user.first_name}, {instance.full_name} wants to connect you!"
-        message = instance.message
-        sender_email = instance.email
+        message = f"Hey {instance.profile.user.first_name},{instance.full_name} wants to connect with you.\n{instance.message}\n Details:\n Full name: {instance.full_name}Email: {instance.email}"
+        sender_email =  settings.DEFAULT_FROM_EMAIL
+        print(sender_email)
         recipient_email = [instance.profile.user.email]
+        print(recipient_email)
         
-        send_mail_enq.delay(subject, message, sender_email, recipient_email)    
-
+        send_mail_talent.delay(subject, message, sender_email, recipient_email)    
+n
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
