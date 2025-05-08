4e9bf77e436926986166c5886a54ecd73d1c328b
mahimaverma <shubhamwadhwa711@gmail.com>
2025-05-08 18:12:21 +0530

    refactor: added new image field

--- Backend/apps/talent/models.py
+++ Backend/apps/talent/models.py
@@ -59,7 +59,9 @@ class Profile(models.Model):
     available_to = models.ManyToManyField(AvailableTo)
     phone = models.CharField(max_length=20, blank=True, null=True)
     linkedin_url = models.URLField(blank=True, null=True)
+    linkedin_profile_url = models.URLField(blank=True, null=True, max_length=1000)
     client = models.ManyToManyField(AssociatedCompany,  related_name='model', through='Client', blank=True, null=True)
+    # sync_complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.first_name 
